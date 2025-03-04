f0d19b5dbad638b6afbf32c8c73b3f9f68ea3c87
Graven | Developpement <gravenilvec@gmail.com>
2025-03-04 16:27:30 +0100

    first

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Voici mes articles
+    <article>Texte 1</article>
+    <article>Texte 2</article>
+    <article>Texte 3</article>
+</body>
+</html>
